d668a858a0f48418a50a28da41f555a8c55e8f7d
Jerzy Ziulkowski <jerry.111@o2.pl>
2023-02-27 20:02:12 +0100

    Add option of text input

--- palindromy.py
+++ palindromy.py
@@ -1,15 +1,37 @@
+
 def palindroms(word):
     """
-       Def palindroms(word) can comparison words
+       Definition of palindroms(word) can comparison words
        which we have and return to us information,
-       that it is or not palindrom
+       that it is palindrom or not. 
     """
-    word = "Kodilla"
+    set_of_word = []
     for letter in word:
-        print(letter)
-    return
-palindroms("kajak")
+        set_of_word.append(letter)
+        print(set_of_word)
+    return set_of_word
 
+
+word_input = []
+word_output = []
+# word_of_multiplication = word_input.intersection(word_output)
+# shopping_B.intersection(shopping_A)
+# print(dir(word_input))
+"""
+def shopping(items, payment='card', shop='local shop'):
+    result = ""
+    result = result + "Idę na zakupy do %s.\n" % shop
+    result = result + "Kupię następujące rzeczy:\n"
+    for item in items:
+        result = result + " - %s\n" % item
+    result = result + "By zapłacić, używam %s." % payment
+    return result
+"""
+if __name__ == "__main__":
+    text = input("Podaj proszę wyraz, aby sprawdzić czy nie jest on palindromem: ")
+    items = text.split(',')
+    shopping_result = palindroms(text)
+    print(shopping_result)
 
 """
 Pamiętaj, że wszystkie zadania, które wysyłasz Mentorowi powinny być umieszczone w Twoim zdalnym repozytorium,
