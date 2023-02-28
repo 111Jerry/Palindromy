595a7af4a9cf3b75fce65729693ca6400517fde5
Jerzy Ziulkowski <jerry.111@o2.pl>
2023-02-28 12:03:22 +0100

    Remove waste from code

--- palindromy.py
+++ palindromy.py
@@ -32,28 +32,4 @@ Podpowiedź
 Pamiętaj, że string/tekst, to kolekcja znaków. Znasz już funkcje kolekcji,
 które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
 Do zadania dodaj krótką dokumentację i umieść je w zdalnym repozytorium. Link prześlij Mentorowi.
-
-def customized_hello(first_name, last_name):
-    print("Hello Mr %s %s" % (first_name, last_name))
-
-customized_hello("John", "Cleese")
-
-def shopping(items):
-    shopping_cart = "Koszyk zawiera: "
-    for item in items:
-        shopping_cart += item + '\n'
-    return shopping_cart
-
-basket = shopping(shopping_items)
-print(basket)
-
-numbers = []
-for i in range(1, 101):
-    if i % 5 == 0:
-        numbers.append(i)
-
-print("Liczby z zakresu od 1 do 100 podzielne przez 5 to:", numbers)
-cube = [number ** 3 for number in numbers]
-print(f"Liczby z listy wyżej podniesione do potęgi trzeciej to {cube}")
-
 """
