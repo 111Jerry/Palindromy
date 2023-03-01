4ead452f2ce3fb9d23f2af5d32f60aedebe846f3
Jerzy Ziulkowski <jerry.111@o2.pl>
2023-03-01 09:19:08 +0100

    Add return True

--- palindromy.py
+++ palindromy.py
@@ -12,10 +12,10 @@ def palindroms(word):
     if word_input != word_output:
         print("Podane przez Ciebie słowo nie jest palindromem!")
     else:
-        print("Podane przez Ciebie słowo nie jest palindromem.")            
-    # print(word_input, word_output)
+        print("Podane przez Ciebie słowo jest palindromem.")            
+    return True 
 if __name__ == "__main__":
-    text = input("Podaj proszę wyraz, aby sprawdzić czy jest on palindromem: ")
+    text = input("Wpisz wyraz, aby sprawdzić czy jest on palindromem:")
     palindroms(text)
 
 
